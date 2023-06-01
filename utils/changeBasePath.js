23f1dd0eba9939ee8803a4f1b52271dddbf92800
Avery knight <averjk@gmail.com>
2023-06-01 15:18:38 -0500

    deploy added

---
+++ utils/changeBasePath.js
@@ -0,0 +1,11 @@
+const ghpages = require('gh-pages');
+const pjson = require('../package.json');
+const fs = require('fs');
+const gitRemoteOriginUrl = (...args) => import('git-remote-origin-url').then(({default: fetch}) => fetch(...args));
+gitRemoteOriginUrl().then(name=>{
+    let vals = name.split('/');
+    let path = `https://${vals[3]}.github.io/${vals[4].split('.')[0]}/`
+    let index = fs.readFileSync('dist/'+pjson.name+'/index.html', 'utf8')
+    fs.writeFileSync('dist/'+pjson.name+'/index.html', index.replace('<base href="/">', `<base href="${path}">`))
+    console.log('Base path changed to',path)
+})
---
+++ utils/deploy.js
@@ -0,0 +1,12 @@
+const ghpages = require('gh-pages');
+const pjson = require('../package.json');
+const gitRemoteOriginUrl = (...args) => import('git-remote-origin-url').then(({default: fetch}) => fetch(...args));
+gitRemoteOriginUrl().then(name=>{
+    let vals = name.split('/');
+    ghpages.publish('dist/'+pjson.name, function(err) {
+        if(typeof err === 'undefined')
+            console.log(`${pjson.name} version ${pjson.version} deployed successfully https://${vals[3]}.github.io/${vals[4].split('.')[0]}` )
+        else
+            console.log(err)
+    });
+})
