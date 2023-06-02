bf026be79a5f8111722679a514cdaf9158c4355a
Mike Tinnes <mtinnes@gmail.com>
2023-06-02 13:30:54 -0500

    change publish pipeline add gadget support to Angular module remove standard hidden prefs from user prefs YAML file

--- utils/changeBasePath.js
+++ utils/changeBasePath.js
@@ -1,11 +1,16 @@
-const ghpages = require('gh-pages');
-const pjson = require('../package.json');
+const pjson = require('../angular.json');
 const fs = require('fs');
 const gitRemoteOriginUrl = (...args) => import('git-remote-origin-url').then(({ default: fetch }) => fetch(...args));
+
 gitRemoteOriginUrl().then(name => {
     let vals = name.split('/');
     let path = `https://${vals[3]}.github.io/${vals[4].split('.')[0]}/`
-    let index = fs.readFileSync('dist/' + pjson.name + '/index.html', 'utf8')
-    fs.writeFileSync('dist/' + pjson.name + '/index.html', index.replace('<base href="/">', `<base href="${path}">`))
-    console.log('Base path changed to', path)
+
+    // Update angular.json baseHref/deployUrl with full path to our app
+    pjson.projects.GadgetHarness.architect.build.configurations.production.baseHref = path;
+    pjson.projects.GadgetHarness.architect.build.configurations.production.deployUrl = path;
+
+    fs.writeFile('./angular.json', JSON.stringify(pjson), function writeJSON(err) {
+        if (err) return console.log(err);
+    });
 })
